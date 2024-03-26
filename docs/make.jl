24c79e1c105c5eee677893b59b8071483a8fac40
nanleij <38724800+nanleij@users.noreply.github.com>
2024-03-26 10:48:24 +0100

    Update make.jl
    actually deploy docs

--- docs/make.jl
+++ docs/make.jl
@@ -35,7 +35,7 @@ makedocs(plugins=[bib],
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-# deploydocs(
-#     repo = "github.com/nanleij/ClusteredLowRankSolver.jl.git",
-#     devbranch="main"
-# )
+deploydocs(
+    repo = "github.com/nanleij/ClusteredLowRankSolver.jl.git",
+    devbranch="main"
+)
